d69eaed504f8e36f8d2ff317c34b425481720d46
hooware <hoowaynew@outlook.com>
2019-04-11 09:56:36 +0800

    剑指offer add 22th solution

--- src/com/hooware/Solution22.java
+++ src/com/hooware/Solution22.java
@@ -1,6 +1,8 @@
 package com.hooware;
 
 import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.Queue;
 
 /**
  * 从上往下打印出二叉树的每个节点，同层节点从左至右打印。
@@ -29,7 +31,20 @@ public class Solution22 {
      */
     public ArrayList<Integer> PrintFromTopToBottom(TreeNode root) {
         ArrayList<Integer> list = new ArrayList<>();
-
+        Queue<TreeNode> queue = new LinkedList<>();
+        if (root != null) {
+            queue.offer(root);
+        }
+        while (!queue.isEmpty()){
+            TreeNode node = queue.poll();
+            list.add(node.val);
+            if (node.left != null){
+                queue.offer(node.left);
+            }
+            if (node.right != null){
+                queue.offer(node.right);
+            }
+        }
         return list;
     }
 }
